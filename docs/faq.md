28d5e8de320fe753b0e715aa27758f51fece93c4
Monkin <hello@monkin.co>
2021-06-01 06:22:07 +0200

    New Documentation Page

---
+++ docs/faq.md
@@ -0,0 +1,31 @@
+---
+id: faq
+sidebar_position: 2
+sidebar_label: FAQ
+slug: /faq
+---
+
+# Frequently Asked Questions
+
+## General Questions
+
+### How to apply for a student license?
+Just send us an e-mail from your university account to [hello@blink.sh](mailto:hello@blink.sh).
+
+### Does Blink Shell work on all Apple devices?
+We support iPhone and iPad, and the newest Mac's with M1 chip.
+
+### Does Blink have a TestFlight version?
+Yes, it has! We call it Community Edition, and you can sign up here: [https://community.blink.sh](https://community.blink.sh)
+
+### How to file a bug report?
+Fill an issue on our [GitHub](https://github.com/blinksh/blink/issues)
+
+## Blink Shell
+
+### What is Blink Window? 
+From iPad OS 13, we have multiple windows support inside Blink, and each window can have infinite tabs. 
+![Blink Shell Windows](faq/windows.png)
+
+### How to enable OSC52 support in Mosh?
+You need to build Mosh from source with this patch: [Support additional OSC 52 clipboard types #1104](https://github.com/mobile-shell/mosh/pull/1104#issuecomment-710754740)
---
+++ docusaurus.config.js
@@ -0,0 +1,143 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Blink Shell',
+  url: 'https://docs.blink.sh',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'blinksh',
+  projectName: 'docs',
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      respectPrefersColorScheme: true,
+      switchConfig: {
+        darkIcon: '  ',
+        darkIconStyle: {
+          marginTop: '1px',
+        },
+        lightIcon: '  ',
+        lightIconStyle: {
+          marginTop: '1px',
+        },
+      },
+    },
+    navbar: {
+      title: 'Blink Shell',
+      logo: {
+        alt: 'Blink Shell',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          type: 'doc',
+          docId: 'basics/tips-and-tricks',
+          position: 'left',
+          label: 'Tips & Tricks',
+        },
+        {
+          href: 'https://github.com/blinksh/blink',
+          className: 'navbar-item-github',
+          position: 'right',
+        },
+        {
+          href: 'https://twitter.com/blinkshell',
+          className: 'navbar-item-twitter',
+          position: 'right',
+        },
+        {
+          type: 'search',
+          position: 'right',
+        },
+      ],
+    },
+    prism: {
+      theme: require('./src/blink'),
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'About',
+          items: [
+            {
+              label: 'Blink Shell',
+                to: 'https://blink.sh',
+            },
+            {
+              label: 'Community Edition',
+                to: 'https://community.blink.sh',
+            },
+          ],
+        },
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Basics',
+              to: 'basics/hosts',
+            },
+            {
+              label: 'Advanced',
+              to: 'advanced/advanced-mosh',
+            },
+            {
+              label: 'Cloud Providers',
+              to: 'cloud-providers/aws',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'GitHub',
+                href: 'https://github.com/blinksh/blink',
+            },
+            {
+              label: 'Discord',
+                href: 'https://discord.gg/ZTtMfvK',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/blinkshell',
+            },
+            {
+              label: 'Reddit',
+              href: 'https://reddit.com/BlinkShell',
+            },
+            {
+              label: 'Dev.to',
+              href: 'https://dev.to/blinkshell',
+            },
+          ],
+        },
+      ],
+        copyright: `Copyright © ${new Date().getFullYear()} Blink Shell is developed by The Blink Shell Project.`,
+    },
+  },
+  plugins: [require.resolve('docusaurus-lunr-search')],
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          editUrl:
+            'https://github.com/blinksh/docs/edit/master/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
